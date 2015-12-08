e50148a10b19db380aba33267a7bfddebdf85ba8
marekborowiec <petiolus@gmail.com>
2015-12-08 18:28:40 -0500

    now negative correlations more visible

--- plotting_correlations.R
+++ plotting_correlations.R
@@ -28,7 +28,7 @@ panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
 {
   usr <- par("usr"); on.exit(par(usr)) 
   par(usr = c(0, 1, 0, 1)) 
-  r <- abs(cor(x, y)) 
+  r <- cor(x, y) 
   txt <- format(c(r, 0.123456789), digits=digits)[1] 
   txt <- paste(prefix, txt, sep="") 
   if(missing(cex.cor)) cex <- 0.99/strwidth(txt) 
@@ -39,7 +39,7 @@ panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
                    cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
                    symbols = c("***", "**", "*", ".", " ")) 
   
-  text(0.5, 0.5, txt, cex = cex * r) 
+  text(0.5, 0.5, txt, cex = cex * abs(r)) 
   text(.8, .8, Signif, cex=cex, col=2)
 }
 
